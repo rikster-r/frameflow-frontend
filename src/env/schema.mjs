6b743137e1007bd98ec2d5ed551ed7e9fdfafd4d
rikster-r <seitdanial43@gmail.com>
2023-02-09 23:27:41 +0600

    Add authentication pages

--- src/env/schema.mjs
+++ src/env/schema.mjs
@@ -25,6 +25,7 @@ export const serverEnv = {
  */
 export const clientSchema = z.object({
   // NEXT_PUBLIC_CLIENTVAR: z.string(),
+  NEXT_PUBLIC_API_HOST: z.string(),
 });
 
 /**
@@ -35,4 +36,5 @@ export const clientSchema = z.object({
  */
 export const clientEnv = {
   // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
+  NEXT_PUBLIC_API_HOST: process.env.NEXT_PUBLIC_API_HOST,
 };
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,19 @@
 import { type AppType } from "next/dist/shared/lib/utils";
-
 import "../styles/globals.css";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <ToastContainer
+        hideProgressBar={true}
+        autoClose={2000}
+        position="bottom-right"
+      />
+    </>
+  );
 };
 
 export default MyApp;
