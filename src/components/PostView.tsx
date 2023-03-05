d961cf82fd0e89400eb488fc969b8734b61cdf8a
rikster-r <seitdanial43@gmail.com>
2023-03-05 21:49:04 +0600

    Fix post controls showing when not logged in

--- src/components/PostView.tsx
+++ src/components/PostView.tsx
@@ -206,22 +206,24 @@ const PostView = ({ user, post, postOwner, comments, path }: Props) => {
             )}
           </>
         )}
-        <button className="ml-auto" onClick={() => setSettingsOpen(true)}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="h-6 w-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M6.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM12.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM18.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
-            />
-          </svg>
-        </button>
+        {user && user.username === postOwner.username && (
+          <button className="ml-auto" onClick={() => setSettingsOpen(true)}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="h-6 w-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M6.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM12.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM18.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
+              />
+            </svg>
+          </button>
+        )}
         <ControlsModal open={settingsOpen} setOpen={setSettingsOpen}>
           <button
             className="w-full py-4 font-semibold text-red-500"
